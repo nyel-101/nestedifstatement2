b2c793cdccf66c56f7958934d11588737bd17f02
nyel-101 <dandevjr@gmail.com>
2025-02-21 08:50:23 +0000

    fix(core): fix database index

--- furry/tests/queries.ts
+++ furry/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d635f56f44d480f8f368bf97e95fd01 = () => {
-  return '2026-05-28T19:49:06Z';
+const node_cc206893c0234adaa513308eaf227152 = () => {
+  return '2025-02-21T08:50:23Z';
 };
