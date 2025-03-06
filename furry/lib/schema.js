d8834391ba046a4841accea198481102d915efb5
nyel-101 <dandevjr@gmail.com>
2025-03-06 19:47:05 +0000

    test(db): restructure session store

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7e229ef2103476988c7a4f79212bf2a = () => {
-  return '2026-04-22T04:55:07Z';
+const node_3d0254bbd1594c7d97c98154aabc7bfe = () => {
+  return '2025-03-06T19:47:05Z';
 };
