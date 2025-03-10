d0b08057f2d35e86f82677e1f70083fbeda5754a
nyel-101 <dandevjr@gmail.com>
2025-03-10 02:50:08 +0000

    refactor(api): fix view templates

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5702fc36c57c41ee9c8c2a266211e24e = () => {
-  return '2026-04-12T05:55:31Z';
+const node_6e37f7b15a834649a3ff7d491bd56dda = () => {
+  return '2025-03-10T02:50:08Z';
 };
