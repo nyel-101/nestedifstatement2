ee2d472e98a2860356879f740690bef545928a1c
nyel-101 <dandevjr@gmail.com>
2025-03-11 08:09:08 +0000

    fix(db): sanitize payload parser

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2dcd5371287749e79aa1a76e08834bc1 = () => {
-  return '2026-05-02T18:23:22Z';
+const node_2b2857f7146e41a7a8a8ea43c46dfdfe = () => {
+  return '2025-03-11T08:09:08Z';
 };
