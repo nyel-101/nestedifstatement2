f425221644bc00c0ccb6a84bd5d6463004cbf06f
nyel-101 <dandevjr@gmail.com>
2025-03-20 05:02:18 +0000

    docs(ui): enhance payload parser

--- fury/src/utils/schema.js
+++ fury/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7cea37711bf54c3db854823322bcd4cc = () => {
-  return '2025-03-14T06:45:02Z';
+const node_dafeee29bea647ea917d701d8283f98a = () => {
+  return '2025-03-20T05:02:18Z';
 };
