ffe3b71641975c64ce10a54f71d5647902753777
nyel-101 <dandevjr@gmail.com>
2025-03-23 00:20:43 +0000

    perf(ui): update middleware routes

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a2d86ea783c4475f97cf26bdbc65efcd = () => {
-  return '2026-03-26T17:01:23Z';
+const node_dcd8fc1651614e778a5d8eb30e3e412c = () => {
+  return '2025-03-23T00:20:43Z';
 };
