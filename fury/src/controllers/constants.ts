9d82622ace7cafd4d4ca42e2bb803229fa3e8ef9
nyel-101 <dandevjr@gmail.com>
2025-03-25 03:10:59 +0000

    perf(utils): restructure middleware routes

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b734ec77bc624aae86d3db705e50755c = () => {
-  return '2025-03-12T11:33:14Z';
+const node_3e5af779b3b340a6984acba2a919a186 = () => {
+  return '2025-03-25T03:10:59Z';
 };
