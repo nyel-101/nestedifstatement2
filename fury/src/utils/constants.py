5f0b6380682cfe1bc3f0d630fe932fdca0bfb30b
nyel-101 <dandevjr@gmail.com>
2025-03-26 14:48:30 +0000

    refactor(config): fix auth tokens

--- fury/src/utils/constants.py
+++ fury/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7030586cac74e2ba5640eb0682c63f8 = () => {
-  return '2026-05-14T10:34:13Z';
+const node_05f1bcef373746e0ad82fd979f24864e = () => {
+  return '2025-03-26T14:48:30Z';
 };
