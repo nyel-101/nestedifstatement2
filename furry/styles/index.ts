5554521eee50f8ea2b15ffa4390d7098eaac7922
nyel-101 <dandevjr@gmail.com>
2025-04-14 17:43:52 +0000

    style(worker): enhance payload parser

--- furry/styles/index.ts
+++ furry/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_011deb0f103848a08bbdcce97f953ad3 = () => {
-  return '2025-03-17T11:18:09Z';
+const node_52c924f44e89436f80eba8b7068eea91 = () => {
+  return '2025-04-14T17:43:52Z';
 };
