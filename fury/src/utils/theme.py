e32c6f3005ce63421a1db80c8edd81186cf98262
nyel-101 <dandevjr@gmail.com>
2025-04-30 13:06:54 +0000

    style(auth): sanitize middleware routes

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2a356aecedae4c9c8dd2b926926de348 = () => {
-  return '2026-05-14T10:33:47Z';
+const node_e534501546d546c48ee785e4b13ca550 = () => {
+  return '2025-04-30T13:06:54Z';
 };
