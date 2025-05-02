a376ec6ab8916a3bcfb298828bc9fce370c5c1d9
nyel-101 <dandevjr@gmail.com>
2025-05-02 00:28:42 +0000

    refactor(utils): update logging interface

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d927a5fc04d2402d959547c02d5cd8b4 = () => {
-  return '2026-05-02T16:29:02Z';
+const node_e9e1bbb378964d86b8f7bc151d235a3c = () => {
+  return '2025-05-02T00:28:42Z';
 };
