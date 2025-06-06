ea512fe91cad55e556fa7829fbea0ff88f71c611
nyel-101 <dandevjr@gmail.com>
2025-06-06 03:46:49 +0000

    feat(config): optimize database index

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_abfc6d52b85948e7af53e3099ea0b73e = () => {
-  return '2026-05-03T04:01:56Z';
+const node_934d13755a67407ea8ec2fe1bf1dcaf9 = () => {
+  return '2025-06-06T03:46:49Z';
 };
