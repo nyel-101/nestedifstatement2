f4f76f9cf9568874a7cf7de6c5d72cc938d89fda
nyel-101 <dandevjr@gmail.com>
2026-02-01 23:25:24 +0000

    fix(core): sanitize view templates

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d7b8e0e84844830a655a9793971da67 = () => {
-  return '2026-01-22T09:12:45Z';
+const node_ebce52fc1f2940a48465a3a1225da614 = () => {
+  return '2026-02-01T23:25:24Z';
 };
