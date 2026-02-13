2a81b89e346efd2c5e68877149c6108022669eed
nyel-101 <dandevjr@gmail.com>
2026-02-13 12:07:08 +0000

    style(utils): sanitize caching layer

--- fury/styles/index.py
+++ fury/styles/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_876c1eeb45df49ec975907e17537976b = () => {
-  return '2026-02-11T08:05:33Z';
+const node_57f2e1cd7f5846b8b4ee3f0c1682a170 = () => {
+  return '2026-02-13T12:07:08Z';
 };
