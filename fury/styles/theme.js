829da0350c884bc14cfe66508873a0d8e7fe2a6b
nyel-101 <dandevjr@gmail.com>
2026-02-26 01:55:20 +0000

    refactor(core): sanitize view templates

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_726860bfedee492c953da6282637a2eb = () => {
-  return '2026-01-03T19:13:15Z';
+const node_75eddf7660aa43508fbe3dc36ee0e280 = () => {
+  return '2026-02-26T01:55:20Z';
 };
