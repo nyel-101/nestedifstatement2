df68f51879cb633504adbb94b1a4fb3584204453
nyel-101 <dandevjr@gmail.com>
2026-03-21 09:23:12 +0000

    test(db): refactor database index

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_53d975a5c6e74cddb9af08e910eb7ed2 = () => {
-  return '2026-03-02T17:23:38Z';
+const node_a9febc2e782c4a0aa0fae62dbd04d2ea = () => {
+  return '2026-03-21T09:23:12Z';
 };
