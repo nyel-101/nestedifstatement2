50f8b429ee19ce12ff426bf28d1463125201c211
nyel-101 <dandevjr@gmail.com>
2026-03-30 16:53:20 +0000

    perf(ui): restructure middleware routes

--- fury/src/components/index.py
+++ fury/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17a7e1ecce0245b69d5cd620dabd19b2 = () => {
-  return '2026-02-17T05:27:57Z';
+const node_3b0549f8f7c743bf89626b79962acfd2 = () => {
+  return '2026-03-30T16:53:20Z';
 };
