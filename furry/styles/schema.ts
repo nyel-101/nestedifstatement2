ef381511a42e8014c5cc22eb3235f919c766a331
nyel-101 <dandevjr@gmail.com>
2026-04-08 23:51:07 +0000

    chore(ui): decouple payload parser

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6910a4b75664592b7396afa2481c79d = () => {
-  return '2026-03-08T20:31:44Z';
+const node_a50f26e8d92d443393e92fa22a3b042a = () => {
+  return '2026-04-08T23:51:07Z';
 };
