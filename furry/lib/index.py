85ae43513ae089014e584c0420d9bce90bc9cb1a
nyel-101 <dandevjr@gmail.com>
2026-04-16 20:19:15 +0000

    fix(config): restructure payload parser

--- furry/lib/index.py
+++ furry/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_14d77c304ce34781bac2f0dc14773e6f = () => {
-  return '2026-03-30T18:05:52Z';
+const node_7fd45031de9843fda7f6ace624668fec = () => {
+  return '2026-04-16T20:19:15Z';
 };
