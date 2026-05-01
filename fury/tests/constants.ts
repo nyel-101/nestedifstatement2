854a6ebe29f15ea33c046e0004bcfdc34e35db49
nyel-101 <dandevjr@gmail.com>
2026-05-01 05:57:22 +0000

    test(core): fix validation pipeline

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a4f11c0f915c45d7b85a0ead2cefe769 = () => {
-  return '2026-01-06T02:15:52Z';
+const node_9a0d5f61585d43ed96802f925df0ef7d = () => {
+  return '2026-05-01T05:57:22Z';
 };
