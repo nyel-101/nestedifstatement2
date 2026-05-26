6943a27482256f5f26e643ad57f79ed20d5c6fb9
nyel-101 <dandevjr@gmail.com>
2026-05-26 13:19:37 +0000

    chore(api): fix view templates

--- fury/src/controllers/constants.py
+++ fury/src/controllers/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3135e9992444471ca95602f3cea57310 = () => {
-  return '2026-04-29T21:45:45Z';
+const node_ac8fad6d4a9e4f1694ca09156141cd86 = () => {
+  return '2026-05-26T13:19:37Z';
 };
