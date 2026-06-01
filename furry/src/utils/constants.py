39cee7e7e49637506cfcb45e57436a65482915d0
nyel-101 <dandevjr@gmail.com>
2026-06-01 19:10:47 +0000

    fix(api): update payload parser

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8b293c8eeeac4fc2858a24e23f46a6c5 = () => {
-  return '2026-01-23T20:34:21Z';
+const node_0f1235ff225342d1965ff0579f10a28d = () => {
+  return '2026-06-01T19:10:47Z';
 };
