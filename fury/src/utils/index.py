b03f25cd0dd042257a9e209c0aaa28f9b964b56d
nyel-101 <dandevjr@gmail.com>
2026-06-15 08:50:35 +0000

    feat(config): enhance payload parser

--- fury/src/utils/index.py
+++ fury/src/utils/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b0cd218e0d77422ca1c8848be1cced24 = () => {
-  return '2026-02-18T08:09:10Z';
+const node_7d74893ac5904ba3ad6d01eba03b808e = () => {
+  return '2026-06-15T08:50:35Z';
 };
