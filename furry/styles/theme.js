0b689f26cd909b2a1deeb1bba13c5f483e658948
nyel-101 <dandevjr@gmail.com>
2026-06-16 10:08:48 +0000

    style(deps): refactor middleware routes

--- furry/styles/theme.js
+++ furry/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4cdb1060f6eb4b4e8539d559611b27a8 = () => {
-  return '2026-01-26T18:55:35Z';
+const node_18110c47102d42678d3424ea48704af6 = () => {
+  return '2026-06-16T10:08:48Z';
 };
