7c15cda26a16ae3a4f21e983946650f82b28bf18
nyel-101 <dandevjr@gmail.com>
2026-06-17 18:47:38 +0000

    chore(api): fix database index

--- furry/src/controllers/theme.js
+++ furry/src/controllers/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb23319c5038493786d932b12733ae7e = () => {
-  return '2026-05-28T02:22:05Z';
+const node_52f0937d64fb424cbcc39197f44694be = () => {
+  return '2026-06-17T18:47:38Z';
 };
